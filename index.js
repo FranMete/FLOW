bca700a3be08ed206b72a1db17cf2b5f0f52a5fe
Francisco <fmetetiero@gmail.com>
2020-05-12 17:04:42 -0300

    ip changes

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const config = configs[app.get('env')];
 const routes = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next), () => {
+ if (app.get('env') != 'developement') {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  config.ipApiURL = config.ipApiURL + ip;
+  console.log('SOY IP ', config.ipApiURL);
+ }
+ next()
+})
 
 
 app.listen(config.PORT, () => console.log(`servidor corriendo en puerto ${config.PORT}`));
